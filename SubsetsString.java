c2667773b3664eabd4f5416f18e8a2321100f2df
lokesh <maturulokesh@gmail.com>
2025-10-24 18:12:28 +0530

    Subsets of String problem recursion

---
+++ SubsetsString.java
@@ -0,0 +1,23 @@
+public class SubsetsString {
+
+    public static void printSubsets(String input, String output, int index){
+
+        // Base case
+        if(index == input.length()){
+            System.out.println(output);
+            return;
+        }
+
+        // Recursive case
+        //1. Include current char
+        printSubsets(input, output + input.charAt(index), index+1);
+
+        //2. Exclude current char
+        printSubsets(input, output, index+1);
+
+    }
+
+    public static void main(String[] args){
+        printSubsets("abcd", "", 0);
+    }
+}
